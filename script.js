6b7665b8e8468fb2ba222a6e16410faf2d35b1ba
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-02-19 19:51:52 -0300

    add criador de botoes

---
+++ script.js
@@ -0,0 +1,76 @@
+const controles = document.getElementById('controles')
+const cssText = document.querySelector('.css')
+const btn = document.querySelector('.btn')
+
+controles.addEventListener('change', handleChange)
+
+const handleStyle = {
+  element: btn,
+  backgroundColor(value){
+    this.element.style.backgroundColor = value;
+  },
+  height(value){
+    this.element.style.height = value + 'px';
+  },
+  width(value){
+    this.element.style.width = value + 'px';
+  },
+  texto(value){
+    this.element.innerText = value;
+  },
+  color(value){
+    this.element.style.color = value;
+  },
+  border(value){
+    this.element.style.border = value;
+  },
+  borderRadius(value){
+    this.element.style.borderRadius = value + 'px';
+  },
+  fontFamily(value){
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value){
+    this.element.style.fontSize = value + 'px';
+  }
+}
+
+function handleChange(event){
+  const name = event.target.name;
+  const value = event.target.value;
+  
+  handleStyle[name](value);
+  saveValues(name, value)
+  showCss()
+}
+
+function saveValues(name, value){
+  localStorage[name] = value
+} 
+
+function setValues(){
+  const properties = Object.keys(localStorage);
+  properties.forEach(propertie => {
+    handleStyle[propertie](localStorage[propertie])
+    controles.elements[propertie].value = localStorage[propertie];
+  })
+  showCss()
+}
+
+setValues();
+
+function showCss(){
+  cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';<span></span>')
+}
+
+  
+function typeWriter(element) {
+    const textoArray = element.innerHTML.split('');
+    element.innerHTML = ' ';
+    textoArray.forEach((letra, index) => {
+        setTimeout(() => element.innerHTML += letra, 131 * index);
+    });
+}
+
+const frase = document.querySelector('.btn-principal')
+typeWriter(frase)
